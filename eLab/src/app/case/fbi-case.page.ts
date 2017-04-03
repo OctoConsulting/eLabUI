3b97a27978344612f988e60d651955e2823835a3
nithinemu <nitin.emmanuel@gmail.com>
2017-04-03 10:58:41 -0400

    wip

--- eLab/src/app/case/fbi-case.page.ts
+++ eLab/src/app/case/fbi-case.page.ts
@@ -2,14 +2,22 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'fbi-case',
-  templateUrl: './fbi-case.page.html'
+  templateUrl: './fbi-case.page.html',
+  styleUrls: ['./fbi-case.page.css']
 })
 export class FbiCasePage implements OnInit{
+  casedata;
   constructor(){
 
   }
 
   ngOnInit(){
-    
+    this.casedata = {
+      Lab_no : '2017-118',
+      Status : 'In Progress',
+      Date_Opened : '3/27/2017 1:40 PM',
+      Violation:  'No',
+      Violation_Date: '3/15/2017 10:22 PM'
+    };
   }
 }
--- eLab/src/app/home/fbi-home.component.ts
+++ eLab/src/app/home/fbi-home.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'fbi-home',
-  templateUrl : './fbi-home.component.html'
+  templateUrl : './fbi-home.component.html',
+  styleUrls: ['./fbi-home.component.css']
 })
 export class FbiHomeComponent implements OnInit{
 
