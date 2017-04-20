309b7b5120a0e36109e1e5f79a3df560b9d091c7
nithinemu <nitin.emmanuel@gmail.com>
2017-04-19 22:38:37 -0400

    wip

--- eLab/src/app/note/fbi-note.component.ts
+++ eLab/src/app/note/fbi-note.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
     selector : 'fbi-note',
@@ -6,11 +7,28 @@ import {Component, OnInit} from '@angular/core';
     styleUrls : ['./fbi-note.component.css']
 })
 export class FBINotePage implements OnInit{
-    constructor(){
+    path : 'view' | 'new' = 'new';
+    type : 'shoe' | 'tire' = 'tire';
+    
+    constructor( private router: Router, private route: ActivatedRoute ){
 
     }
 
     ngOnInit(){
-
+        this.determinePath();
+        this.determineType();
     }
+
+    determinePath() {
+        if (/\/view/.test(this.router.url)) {
+            this.path = 'view';
+        }
+    }
+
+    determineType(){
+        if (/\/shoe/.test(this.router.url)) {
+            this.type = 'shoe';
+        }
+    }
+
 }
