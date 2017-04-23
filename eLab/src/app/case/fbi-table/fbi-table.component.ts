4488a5d76cef1ddfeba484fad8af371110d4ff1d
nithinemu <nitin.emmanuel@gmail.com>
2017-04-22 22:25:15 -0400

    table fixed

--- eLab/src/app/case/fbi-table/fbi-table.component.ts
+++ eLab/src/app/case/fbi-table/fbi-table.component.ts
@@ -1,79 +1,74 @@
-import {Component, OnInit,Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
 import { FbiEvidenceService } from '../../api-kit/evidences/fbi-evidences.service';
 
 @Component({
   selector: 'fbi-table',
-  templateUrl : "./fbi-table.template.html",
-  styleUrls : ['./fbi-table.component.css']
+  templateUrl: "./fbi-table.template.html",
+  styleUrls: ['./fbi-table.component.css']
 })
-export class FBITable implements OnInit{
+export class FBITable implements OnInit {
   evidenceTypes = [];
   flag = false;
   tabData;
-  checked : any = [];
+  checked: any = [];
 
   @Input() evidenceContainer;
 
-  heading:{
-    tableHeading : string,
-    mainHeading : any,
-    addButton : string,
+  heading: {
+    tableHeading: string,
+    mainHeading: any,
+    addButton: string,
   };
 
-  constructor(private evidence : FbiEvidenceService){
-    
+  constructor(private router: Router, private route: ActivatedRoute,private evidence: FbiEvidenceService) {
+
   }
 
-  ngOnInit(){
-    
+  ngOnInit() {
+
     this.heading = {
-      tableHeading : 'Evidence',
-      mainHeading : ['Type', 'Number', 'Name','For Analysis'],
-      addButton : 'Create Evidence',
+      tableHeading: 'Evidence',
+      mainHeading: ['Type', 'Number', 'Name', 'For Analysis'],
+      addButton: 'Create Evidence',
     };
 
     this.getEvidenceTypes();
-    
+
   }
 
-  checkFlag(){
+  checkFlag() {
     this.flag = !this.flag;
-    return this.flag;    
+    return this.flag;
   }
 
-  checkEvidenceType(type : number){
+  checkEvidenceType(type: number) {
     let value = '';
     this.evidenceTypes.forEach(types => {
-      console.log(types.id + "," + type)
-      if(types.id === type){
-        console.log(type);
+      if (types.id === type) {
         value = types.description;
       }
     });
 
     return value;
-
-    // if(type === 1)
-    //   return "Container";
-    // else if(type === 2)
-    //   return "Package";
-    // else  
-    //   return "Item";
   }
 
-  getEvidenceTypes(){
+  getEvidenceTypes() {
     this.evidence.getEvidenceTypes().subscribe(res => {
       this.evidenceTypes = res;
     });
   }
 
-  OnChange(event,flag){
-    
+  OnChange(event, flag) {
     //console.log("Clicked " + event + " , " +  flag);
-    this.evidence.updateForAnalysis(event,flag).subscribe( res => {
+    this.evidence.updateForAnalysis(event, flag).subscribe(res => {
       //console.log(res);
     });
   }
 
+  viewEvidence(event) {
+    this.router.navigate(['./evidence/view/',event]);
+    window.scrollTo(0,0);    
+  }
 
 }
