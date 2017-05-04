a5210c6d8c426fa04a18de5c4e715664a31c0934
Zane <i.zane.am.robot@gmail.com>
2017-05-04 17:25:11 -0400

    some progress with a dropdown navbar button

--- eLab/src/app/fbi-sidenav/fbi-sidenav.component.ts
+++ eLab/src/app/fbi-sidenav/fbi-sidenav.component.ts
@@ -20,4 +20,25 @@ export class FBISideNav implements OnInit{
     this.path = event;
     //console.log(event);
   }
+
+  /* When the user clicks on the button, 
+  toggle between hiding and showing the dropdown content */
+  showCaseDetailsDropdown() {
+    $(".dropdown-content").toggleClass('hidden');
+  }
+  
+  /* 
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = $(".dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }*/
 }
